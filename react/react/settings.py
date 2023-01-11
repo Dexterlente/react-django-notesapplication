91810e15e0bc051a25584b78aae1c9c4be6f0853
Dexterlente <dexter.lente18@gmail.com>
2023-01-12 00:40:34 +0800

    s

--- react/react/settings.py
+++ react/react/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #App installation
     'post.apps.PostConfig',
+    #DRF install
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
